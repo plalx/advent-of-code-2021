cb2a4362eae607ab7c0c4796b286745d39e9c63c
plalx <903992+plalx@users.noreply.github.com>
2021-12-12 21:39:20 -0500

    D1/P2: Refactor using `WindowedSum`

--- day1/solve-part-2.mjs
+++ day1/solve-part-2.mjs
@@ -6,23 +6,65 @@ export function solvePart2(measurements) {
     }
 
     let increments = 0;
-    let windowStart = 0;
-    let previousWindowSum = 0;
+    const windowedSum = WindowedSum.over(measurements, windowSize);
+    let previousSum = windowedSum.sum;
 
-    for (let i = 0; i < windowSize; i++) {
-        const measurement = measurements[i];
-        previousWindowSum += measurement;
-    }
-
-    for (let i = windowSize; i < measurements.length; i++) {
-        let currentWindowSum = previousWindowSum;
-        currentWindowSum -= measurements[windowStart++];
-        currentWindowSum += measurements[i];
-
-        if (currentWindowSum > previousWindowSum) increments++;
-        
-        previousWindowSum = currentWindowSum;
+    while (windowedSum.hasNext()) {
+        windowedSum.next();
+        if (windowedSum.sum > previousSum) increments++;
+        previousSum = windowedSum.sum;
     }
 
     return increments;
+}
+
+class WindowedSum {
+    #items;
+    #start;
+    #end;
+    #sum;
+
+    constructor(items, size) {
+        this.#items = items;
+        this.#start = 0;
+        this.#end = -1;
+        
+        this.#initializeSum(Math.min(size, items.length));
+    }
+
+    #initializeSum(size) {
+        this.#sum = 0;
+        for (let i = 1; i <= size; i++) {
+            this.#addItem();
+        }
+    }
+
+    #addItem() {
+        this.#sum += this.#items[++this.#end];
+    }
+
+    #removeItem() {
+        this.#sum -= this.#items[this.#start++];
+    }
+
+    hasNext() {
+        return this.#end < this.#items.length;
+    }
+
+    next() {
+        if (!this.hasNext()) {
+            throw new Error("Can't slide window to next position: end reached.");
+        }
+
+        this.#removeItem();
+        this.#addItem();
+    }
+
+    get sum() {
+        return this.#sum;
+    }
+
+    static over(items, size) {
+        return new WindowedSum(items, size);
+    }
 }
